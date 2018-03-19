78962c253becc324a50c3547085be8049445bd80
Aki Kutvonen <aki.kutvonen@gmail.com>
2018-03-19 17:09:27 +0900

    Update digitgenerator_main.py

--- digitgenerator/digitgenerator_main.py
+++ digitgenerator/digitgenerator_main.py
@@ -5,7 +5,7 @@ from collections import defaultdict
 import pickle
 import matplotlib
 import matplotlib.pyplot as plt
-from digitgenerator import Vae
+from .VAE import Vae
 
 #this file contains the Digitgenerator class and it's methods and properties
 
